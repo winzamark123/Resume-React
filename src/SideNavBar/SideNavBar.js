045d24c0bab390c3dac9a8160d96da2d380c8076
Win Cheng <wincheng@Wins-MacBook-Pro.local>
2023-04-29 20:01:50 -0700

    Started SideNavBar

---
+++ src/SideNavBar/SideNavBar.js
@@ -0,0 +1,19 @@
+import "./SideNavBar.css"
+
+const sideNavBar = () => {
+    return (
+        <div className="side-nav-container">
+            <div className="nav-upper">
+                <div className="nav-heading">
+                    <div className="nav-brand">
+                        {/* <img src="Icons/" alt="" /> Logo for my Profile */}
+                        <h2>ShowKart</h2>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+
+        </div>);
+}
+
+export default sideNavBar;
