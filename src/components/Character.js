4631d0a4a729ade839a190cd32f366741508b3c1
Win Cheng <wincheng@Wins-MacBook-Pro.local>
2023-07-18 00:31:09 -0700

    Build; Created 4 Characters that are able to move independently

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,32 +2,44 @@ import "./Character.css"
 import { useState, useRef, useEffect, useCallback } from 'react';
 import FrogCharacter from '../assets/Pixel Art Characters/Main Characters/Ninja Frog/Idle (32x32).png'
 import NinjaCharacter from '../assets/Pixel Art Characters/Main Characters/Mask Dude/Idle (32x32).png'
+import PinkCharacter from '../assets/Pixel Art Characters/Main Characters/Pink Man/Idle (32x32).png'
+import VirtualCharacter from '../assets/Pixel Art Characters/Main Characters/Virtual Guy/Idle (32x32).png'
 
 
-const Character = ({ x, y, setX, setY }) => {
+const Character = ({ characterName, initPos, isActive, activeCharacterIndex }) => {
+
+    const characterImages = {
+        "Frog": FrogCharacter,
+        "Ninja": NinjaCharacter,
+        "Pink": PinkCharacter,
+        "Virtual": VirtualCharacter
+    };
+
     var speed = 3;
 
     const characterRef = useRef(null);
 
-    // const [x, setX] = useState(0);
-    // const [y, setY] = useState(0);
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
     const [facing, setFacing] = useState("down");
     const [walking, setWalking] = useState(false);
-
     var [heldDirectionsArray, setHeldDirectionsArray] = useState([]);
 
+    useEffect(() => {
+        if (initPos) {
+            setX(initPos.x);
+            setY(initPos.y);
+        }
+    }, [initPos]);
+
     const directions = {
-        up: "up",
-        down: "down",
         left: "left",
         right: "right"
     }
 
     const keys = {
-        // 38: directions.up,
         37: directions.left,
         39: directions.right,
-        // 40: directions.down
     }
 
     const placeCharacter = useCallback(() => {
@@ -40,19 +52,13 @@ const Character = ({ x, y, setX, setY }) => {
         if (currentDirection === directions.left) {
             setX(x => x - speed);
         }
-        if (currentDirection === directions.up) {
-            setY(y => y - speed);
-        }
-        if (currentDirection === directions.down) {
-            setY(y => y + speed);
-        }
 
         setFacing(currentDirection); //set facing to the current direction
         setWalking(currentDirection ? true : false); //set walking to true if the current direction is anything other than null
 
         // mapRef.current.style.transform = `translate3d(${-x * pixelSizeNumber}px, ${-y * pixelSizeNumber}px, 0 )`;
         characterRef.current.style.transform = `translate3d(${x}px, ${y}px, 0)`;
-    }, [x, y, directions, setX, setY, setFacing, setWalking, heldDirectionsArray]);
+    }, [x, directions, setX, setFacing, setWalking, heldDirectionsArray]);
 
     const step = useCallback(() => {
         placeCharacter();
@@ -64,9 +70,9 @@ const Character = ({ x, y, setX, setY }) => {
 
     useEffect(() => {
         const handleKeyDown = (e) => {
-            console.log(e.keyCode);
+            // console.log(e.keyCode);
             var dir = keys[e.keyCode];
-            if (dir && heldDirectionsArray.indexOf(dir) === -1) {
+            if (dir && heldDirectionsArray.indexOf(dir) === -1 && isActive) {
                 //might have to change this line as the array is being expanded from the front 
                 setHeldDirectionsArray((prev) => [dir, ...prev]);
             }
@@ -83,7 +89,7 @@ const Character = ({ x, y, setX, setY }) => {
         const handleKeyUp = (e) => {
             var dir = keys[e.keyCode];
             var index = heldDirectionsArray.indexOf(dir);
-            if (index > -1) {
+            if (index > -1 && isActive) {
                 setHeldDirectionsArray((prev) => [...prev.slice(0, index), ...prev.slice(index + 1)]);
             }
         }
@@ -104,7 +110,10 @@ const Character = ({ x, y, setX, setY }) => {
     return (
         <div className="character-container">
             <div ref={characterRef} className="character" facing={facing} walking={walking}>
-                <img src={FrogCharacter} className="character_spritesheet" alt="" />
+                <img src={characterImages[characterName]} className="character_spritesheet" alt="" />
+                <img src={characterImages[characterName]} className="character_spritesheet" alt="" />
+                <img src={characterImages[characterName]} className="character_spritesheet" alt="" />
+                <img src={characterImages[characterName]} className="character_spritesheet" alt="" />
             </div>
         </div>
     );
--- src/components/Game.js
+++ src/components/Game.js
@@ -11,14 +11,26 @@ const Game = () => {
     const [characterNames] = useState(["Frog", "Ninja", "Pink", "Virtual"]);
     const [activeCharacterIndex, setActiveCharacterIndex] = useState(0);
 
+    const initPos = {
+        "Frog": { x: 0, y: 0 },
+        "Ninja": { x: 0, y: 100 },
+        "Pink": { x: 0, y: 200 },
+        "Virtual": { x: 0, y: 300 },
+    };
+
     useEffect(() => {
         const handleKeyDown = (e) => {
             if (e.keyCode === 38) {
-                setActiveCharacterIndex(activeCharacterIndex => (activeCharacterIndex - 1) % characterNames.length);
+                setActiveCharacterIndex(activeCharacterIndex => ((activeCharacterIndex - 1) % characterNames.length) < 0 ? 0 : (activeCharacterIndex - 1) % characterNames.length);
+                console.log(activeCharacterIndex);
             }
 
             if (e.keyCode === 40) {
-                setActiveCharacterIndex(activeCharacterIndex => (activeCharacterIndex + 1) % characterNames.length);
+                if (e.keyCode === 40) {
+                    setActiveCharacterIndex(activeCharacterIndex => activeCharacterIndex < characterNames.length - 1 ? activeCharacterIndex + 1 : activeCharacterIndex);
+                }
+
+                console.log(activeCharacterIndex);
             }
         }
 
@@ -30,15 +42,21 @@ const Game = () => {
 
     }, [activeCharacterIndex, characterNames.length]);
 
-
-
-
-
     return (
         <div className="game-container">
             <div className="frame">
                 <Map x={x} y={y} />
-                <Character x={x} y={y} setX={setX} setY={setY} />
+                {characterNames.map((characterName, index) => (
+                    <Character
+                        key={characterName}
+                        characterName={characterName}
+                        activeCharacterIndex={activeCharacterIndex}
+                        isActive={activeCharacterIndex === index}
+                        initPos={initPos[characterName]}
+                        setX={setX}
+                    />
+                ))}
+                <Character x={x} y={y} setX={setX} activeCharacterIndex={activeCharacterIndex} />
                 {/* <Item x={x} y={y} /> */}
             </div>
         </div>
