b6d6e456bb14c9830c0c12e63dbcf9dc5540b159
Win Cheng <wincheng@Wins-MacBook-Pro.local>
2023-07-29 18:02:35 -0700

    Build; HomePage Structure Improvements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,17 @@ import './Home.scss';
 const home = () => {
     return (
         <div className="home">
-            <div className="home_header">
-            </div>
+            {/* <div className="home_header">
+            </div> */}
 
             <div className="home_body">
                 <div className="home_body_left">
                     <div className="home_body_left_title">
-                        <h1> Hi, I'm </h1>
-                        <span className="home_body_left_title_name">
-                            TEERANADE (WIN) CHENG 
-                        </span>
+                        <h1> Teeranade Cheng </h1>
+                        <span> Aspriring Software Engineer</span>
                     </div>
+                </div>
+                <div className="home_body_right">
                 </div>
             </div>
         </div>
