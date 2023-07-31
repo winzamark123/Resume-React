b910274fb0b8061711c91f78bb1e320d0c1f324b
Win Cheng <winzamark12@gmail.com>
2023-07-31 16:22:38 -0700

    Build; HomePage; Social Media Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './Home.scss';
 
 const Home = () => {
@@ -21,7 +23,7 @@ const Home = () => {
     // Update margin on string length change
     useEffect(() => {
         // Customize this calculation according to your needs
-        const newWidth= listItems[currIndex].length * 40; // for example
+        const newWidth = listItems[currIndex].length * 40; // for example
         setAnimateTextWidth(newWidth);
     }, [currIndex, listItems]);
 
@@ -38,16 +40,44 @@ const Home = () => {
                     <div className="home_body_left_title">
                         <div className="home_body_left_title_animate">
                             <div className="home_body_left_title_animate_text" > Aspiring </div>
-                            <ul className = "home_body_left_title_animate_list">
-                                <li className = "home_body_left_title_animate_list_item">Software Engineer</li>
-                                <li className = "home_body_left_title_animate_list_item">Full-Stack Developer</li>
-                                <li className = "home_body_left_title_animate_list_item">CEO of my own Company</li>
-                                <li className = "home_body_left_title_animate_list_item">Anime Addict</li>
+                            <ul className="home_body_left_title_animate_list">
+                                <li className="home_body_left_title_animate_list_item">Software Engineer</li>
+                                <li className="home_body_left_title_animate_list_item">Full-Stack Developer</li>
+                                <li className="home_body_left_title_animate_list_item">CEO of my own Company</li>
+                                <li className="home_body_left_title_animate_list_item">Anime Addict</li>
                             </ul>
                         </div>
                         <h1 className='home_body_left_title_name_firstName'> Teeranade </h1>
                         <h2 className='home_body_left_title_name_lastName'> CHENG</h2>
                     </div>
+
+                    <div className="socials_container">
+                        <ul className="socials_container_list">
+                            <li className="socials_container_list_item">
+                                <a href="https://www.instagram.com/winzamark12/">
+                                    <button class="button-logo">
+                                        <FontAwesomeIcon icon={faInstagram} size="5x" />
+                                    </button>
+                                </a>
+                            </li>
+                            <li className="socials_container_list_item">
+                                <a href="https://github.com/winzamark123">
+                                    <button class="button-logo">
+                                        <FontAwesomeIcon icon={faGithub} size="5x" />
+                                    </button>
+                                </a>
+                            </li>
+                            <li className="socials_container_list_item">
+                                <a href="https://www.linkedin.com/in/teeranade-cheng-68ab28253/">
+                                    <button class="button-logo">
+                                        <FontAwesomeIcon icon={faLinkedin} size="5x" />
+                                    </button>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+
                 </div>
                 <div className="home_body_right">
                 </div>
@@ -57,4 +87,4 @@ const Home = () => {
 }
 
 export default Home;
-//Just trying out Commits gg
+
