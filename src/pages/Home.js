e0afc630ae1878af2beeaf55b15efd80089dae90
Win Cheng <winzamark12@gmail.com>
2023-07-31 01:58:42 -0700

    Build; Animated Test Aspiring...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,52 @@
+import { useState, useEffect } from 'react';
 import './Home.scss';
 
-const home = () => {
+const Home = () => {
+
+    const listItems = ["Software Engineer",
+        "Full-Stack Developer",
+        "Anime Addict",
+        "CEO of my own Company"];
+
+    const [currIndex, setCurrIndex] = useState(0);
+    const [animateTextWidth, setAnimateTextWidth] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrIndex(currIndex => (currIndex + 1) % listItems.length);
+        }, 3000);
+        return () => clearInterval(interval);
+    }, [listItems]);
+
+    // Update margin on string length change
+    useEffect(() => {
+        // Customize this calculation according to your needs
+        const newWidth= listItems[currIndex].length * 40; // for example
+        setAnimateTextWidth(newWidth);
+    }, [currIndex, listItems]);
+
+    const widthStyle = { width: `${animateTextWidth + 80}px` };
+
+
+
+
     return (
         <div className="home">
-            {/* <div className="home_header">
-            </div> */}
 
             <div className="home_body">
                 <div className="home_body_left">
                     <div className="home_body_left_title">
-                        <h1> Teeranade Cheng </h1>
-                        <span> Aspriring Software Engineer</span>
+                        <div className="home_body_left_title_animate">
+                            <div className="home_body_left_title_animate_text" > Aspiring </div>
+                            <ul className = "home_body_left_title_animate_list">
+                                <li className = "home_body_left_title_animate_list_item">Software Engineer</li>
+                                <li className = "home_body_left_title_animate_list_item">Full-Stack Developer</li>
+                                <li className = "home_body_left_title_animate_list_item">CEO of my own Company</li>
+                                <li className = "home_body_left_title_animate_list_item">Anime Addict</li>
+                            </ul>
+                        </div>
+                        <h1 className='home_body_left_title_name_firstName'> Teeranade </h1>
+                        <h2 className='home_body_left_title_name_lastName'> CHENG</h2>
                     </div>
                 </div>
                 <div className="home_body_right">
@@ -20,5 +56,5 @@ const home = () => {
     );
 }
 
-export default home;
-//Just trying out Commits 
+export default Home;
+//Just trying out Commits gg
