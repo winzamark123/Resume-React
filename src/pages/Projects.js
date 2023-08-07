f871f850967de97899638ca7239b1c7e7bacddf4
Win Cheng <winzamark12@gmail.com>
2023-08-07 02:17:36 -0700

    Build; Projects Page added FinanceAuto

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,7 @@
 import "./Projects.scss"
 import tasaBG from "../assets/Projects-BG/TASA-bg.png"
 import imageGPTBG from "../assets/Projects-BG/NewImageGPT-bg.png"
+import financeAutoBG from "../assets/Projects-BG/FinanceAuto-bg.png"
 
 const CarouselItem = ({ order, style }) => {
 
@@ -31,8 +32,8 @@ const Projects = () => {
                 </div>
                 <div className="projects_container_carousel">
                     <CarouselItem order={1} style={{ backgroundImage: `url(${imageGPTBG})`, backgroundSize: "120%", backgroundPosition: "50% 50%", }} />
-                    <CarouselItem order={2} style={{ backgroundImage: `url(${tasaBG})`, backgroundSize: "180%", backgroundPosition: "50% 90%" }} />
-                    <CarouselItem order={3} style={{ backgroundImage: `url(${tasaBG})` }} />
+                    <CarouselItem order={2} style={{ backgroundImage: `url(${tasaBG})`, backgroundSize: "180%", backgroundPosition: "50% 100%" }} />
+                    <CarouselItem order={3} style={{ backgroundImage: `url(${financeAutoBG})`, backgroundSize: "170%", backgroundPosition: "5% 50%", backgroundRepeat: "no-repeat", }} />
                 </div>
             </div>
         </div>
