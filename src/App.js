df62d0c611eb3c196923742823919f6f5f020f54
Win Cheng <winzamark12@gmail.com>
2023-08-20 17:09:38 +0700

    Map Component; Outline of clickable areas and cursor div in App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import Game from './pages/Game';
 import NavBar from './components/NavBar';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [cursorX, setCursorX] = useState(0);
+  const [cursorY, setCursorY] = useState(0);
+
+  useEffect(() => {
+    const handleCursor = (event) => {
+      setCursorX(event.clientX);
+      setCursorY(event.clientY);
+    }
+  })
+
+
   return (
     <div className="App">
       {/* <NavBar /> */}
+      <div id="cursor" style={{ left: `${cursorX}px`, right: `${cursorY}` }}></div>
       <Home />
       <Game />
       <Projects />
       {/* <Contact /> */}
-      
+
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -7,13 +7,21 @@ const Map = () => {
             <div className="map-pixelart">
                 <div className="floor" id="floor1"></div>
                 <div className="floor" id="floor2">
-                    <div className="floor_door">
+                    <div className="floor_hover" id = "door">
+
+                    </div>
+                </div>
+                <div className="floor" id="floor3">
+                    <div className="floor_hover" id = "livingRoom">
+
+                    </div>
+                </div>
+                <div className="floor" id="floor4"></div>
+                <div className="floor" id="floor5">
+                    <div className="floor_hover" id = "painting">
                         
                     </div>
                 </div>
-                <div className="floor" id="floor3"></div>
-                <div className="floor" id="floor4"></div>
-                <div className="floor" id="floor5"></div>
             </div>
         </div>
     );
