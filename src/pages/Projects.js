9a859e2488df574d9308c8fc191ca26cafabb8f8
Win Cheng <winzamark12@gmail.com>
2023-09-03 13:51:49 +0800

    ProjectsPage; Finished Structure

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -21,17 +21,19 @@ const backgroundImageStyles = [
     }
 ];
 
-const itemLabelStyles = [
+const itemLabels = [
     {
         title: "ImageGPT",
         descrip: "ImageGPT: An advanced GPT-3 model trained to convert visual data into actionable text content, enabling dynamic queries on image-derived contexts.",
-        backgroundImageStyles: `${backgroundImageStyles[0]}`
+        backgroundImageStyles: `${backgroundImageStyles[0]}`,
+        marginLeft: "150px"
     },
 
     {
         title: "TASA Website",
         descrip: "A website for the Thai American Student Association at UCSD, built with React and Firebase.",
-        backgroundImageStyles: `${backgroundImageStyles[1]}`
+        backgroundImageStyles: `${backgroundImageStyles[1]}`,
+        marginLeft: "200px"
     },
 
     {
@@ -93,10 +95,6 @@ const Projects = ({ cursorRef }) => {
                     cursorRef.current.style.height = "100px";
                     cursorRef.current.style.border = "3px solid #16a085";
             }
-
-            // cursorRef.current.style.width = "100px";
-            // cursorRef.current.style.height = "100px";
-            // cursorRef.current.style.border = "3px solid #16a085";
         }
     }
 
@@ -117,20 +115,39 @@ const Projects = ({ cursorRef }) => {
                         <div className="projects_container_carousel_floor_hover"
                             onMouseEnter={handleMouseEnter}
                             onMouseLeave={handleMouseLeave}
-                            id = {itemLabelStyles[0]["title"]} //id = ImageGPT
-                        >
-
-                        </div>
-                    </div>
-                    <div className="projects_container_carousel_floor" id="floor2">
+                            id={itemLabels[0]["title"]} //id = ImageGPT
+                            style={{ marginLeft: "10vw" }}
+                        ></div>
 
                     </div>
-                    <div className="projects_container_carousel_floor" id="floor3"></div>
+
+                    <div className="projedcts_container_carousel_floor" id="floor2">
+                        <div className="projects_container_carousel_floor_hover"
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                            id={itemLabels[1]["title"]}
+                            style={{ marginLeft: "12vw" }}
+                        ></div>
+                    </div>
+                    <div className="projects_container_carousel_floor" id="floor3">
+
+                    </div>
+                    <div className="projects_container_carousel_floor" id="floor4">
+                        <div className="projects_container_carousel_floor_hover"
+                            onMouseEnter={handleMouseEnter}
+                            onMouseLeave={handleMouseLeave}
+                            id={itemLabels[2]["title"]}
+                            style={{ marginLeft: "50vw" }}
+                        ></div>
+                    </div>
                 </div>
             </div>
             <div className="projects_title">
                 <span>Some of my not so cool </span>
                 <h1>Projects</h1>
+            </div>
+            <div className="projects_items">
+                
             </div>
         </div>
 
