751327908e9855a57db5a9c819d65d601eed8a7a
Win Cheng <winzamark12@gmail.com>
2023-09-23 17:26:29 -0700

    Map Component; Background Image Updated BGColor Updated

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,44 +4,6 @@ import Map from '../components/Map';
 import Character from '../components/Character';
 
 const Game = ({cursorRef}) => {
-    var [x, setX] = useState(0);
-    var [y, setY] = useState(0);
-
-    const [characterNames] = useState(["Frog", "Ninja", "Pink", "Virtual"]);
-    const [activeCharacterIndex, setActiveCharacterIndex] = useState(0);
-
-    const initPos = {
-        "Frog": { x: 0, y: 0 },
-        "Ninja": { x: 0, y: 100 },
-        "Pink": { x: 0, y: 200 },
-        "Virtual": { x: 0, y: 300 },
-    };
-
-    //Change Characters Index based on the KeyPress -> Mouse YPosition
-    // useEffect(() => {
-    //     const handleKeyDown = (e) => {
-    //         if (e.keyCode === 38) {
-    //             setActiveCharacterIndex(activeCharacterIndex => ((activeCharacterIndex - 1) % characterNames.length) < 0 ? 0 : (activeCharacterIndex - 1) % characterNames.length);
-    //             console.log(activeCharacterIndex);
-    //         }
-
-    //         if (e.keyCode === 40) {
-    //             if (e.keyCode === 40) {
-    //                 setActiveCharacterIndex(activeCharacterIndex => activeCharacterIndex < characterNames.length - 1 ? activeCharacterIndex + 1 : activeCharacterIndex);
-    //             }
-
-    //             console.log(activeCharacterIndex);
-    //         }
-    //     }
-
-    //     window.addEventListener("keydown", handleKeyDown);
-
-    //     return () => {
-    //         window.removeEventListener("keydown", handleKeyDown);
-    //     }
-
-    // }, [activeCharacterIndex, characterNames.length]);
-
     return (
         <div className="game_container">
             <div className="frame">
